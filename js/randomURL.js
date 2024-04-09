8ab2417d24ada8ff387ce9f15706ef1dd535fdce
af-ad <152578450+af-ad@users.noreply.github.com>
2024-04-09 21:03:30 +0900

    Update randomURL.js

--- js/randomURL.js
+++ js/randomURL.js
@@ -3,3 +3,13 @@ function getURLString(url) {
     const random = Math.floor(Math.random()*range)
     return url[random].url
 }
+//버전2
+function getRandomURL(vodArray){
+    let range=vodArray.length;
+    const random = Math.floor(Math.random()*range);
+    const playList=Object.keys(vodArray[random]);
+    range=vodArray[random][playList].length;
+    const vodindex=Math.floor(Math.random()*range);
+    const urlString=`https://vod.afreecatv.com/player/${vodArray[random][playList][vodindex]}/playlist/${playList}`;
+    return urlString
+}
